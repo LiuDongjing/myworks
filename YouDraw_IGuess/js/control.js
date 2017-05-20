4aa33eafbb9c387a108933d32390fe03b189040e
LiuDongjing <zhuce201602@yeah.net>
2017-05-20 16:46:14 +0800

    你画我猜的基本客户端

---
+++ YouDraw_IGuess/js/control.js
@@ -0,0 +1,15 @@
+function init() {
+	var customBoard2 = new DrawingBoard.Board('paint', {
+		controls: [
+			'Color',
+			{ Size: { type: 'dropdown' } },
+			{ DrawingMode: { filler: true } },
+			'Navigation',
+			'Download'
+		],
+		size: 2,
+		background: "#fff",
+		webStorage: 'session',
+		enlargeYourContainer: true
+	});
+}
