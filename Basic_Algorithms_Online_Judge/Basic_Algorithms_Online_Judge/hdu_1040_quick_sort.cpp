b5bd4e66a1daba370b4805860864e6f7fe544eb4
LiuDongjing <zhuce201602@yeah.net>
2017-07-25 20:42:06 +0800

    添加了基础算法练习

---
+++ Basic_Algorithms_Online_Judge/Basic_Algorithms_Online_Judge/hdu_1040_quick_sort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+using namespace std;
+int partition(vector<int> &data, int start, int end) {
+
+}
+int main() {
+	srand(time(0));
+	int cases;
+	cin >> cases;
+	for (; cases > 0; cases--) {
+		int n;
+		cin >> n;
+		vector<int> data(n);
+		for (int i = 0; i < n; i++)
+			cin >> data[i];
+		
+	}
+
+}
