7b6046d94d295025cde07e1476d8f8005d760c62
LiuDongjing <zhuce201602@yeah.net>
2017-07-28 13:03:59 +0800

    还是tle

--- Basic_Algorithms_Online_Judge/Basic_Algorithms_Online_Judge/spoj_binary_search.cpp
+++ Basic_Algorithms_Online_Judge/Basic_Algorithms_Online_Judge/spoj_binary_search.cpp
@@ -5,36 +5,30 @@ int main() {
 #ifdef _DEBUG
 	freopen("spoj_binary_search.txt", "r", stdin);
 #endif
-	int n, q;
+	long long n, q;
 	cin >> n >> q;
-	vector<int> data(n);
-	for (int i = 0; i < n; i++)
+	vector<long long> data(n);
+	for (long long i = 0; i < n; i++)
 		cin >> data[i];
-	for (int i = 0; i < q; i++) {
-		int e;
-		int index = -1;
-		int low = 0, high = n;
+	for (long long i = 0; i < q; i++) {
+		long long e;
+		long long index = -1;
+		long long low = 0, high = n-1;
 		cin >> e;
-		while (low < high) {
-			int m = (low + high) / 2;
+		while (low <= high) {
+			long long m = (low + high) / 2;
 			if (data[m] < e) {
 				low = m + 1;
 			}
 			else if (data[m] > e) {
-				high = m;
+				high = m - 1;
 			}
 			else {
-				if (m - 1 >= 0 && data[m - 1] == e) {
-					//不是第一次出现
-					high = m;
-				}
-				else {
-					index = m;
-					break;
-				}
+				high = m - 1;
+				index = m;
 			}
 		}
-		cout << index << endl << endl;
+		cout << index << endl;
 	}
 	return 0;
 }
