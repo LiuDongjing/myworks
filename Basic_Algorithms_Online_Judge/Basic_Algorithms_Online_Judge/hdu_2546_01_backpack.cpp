18a70ebb639a7efa9f3d2a43fc95992158da80f7
LiuDongjing <zhuce201602@yeah.net>
2017-09-29 12:37:29 +0800

    hdu 2546 AC

--- Basic_Algorithms_Online_Judge/Basic_Algorithms_Online_Judge/hdu_2546_01_backpack.cpp
+++ Basic_Algorithms_Online_Judge/Basic_Algorithms_Online_Judge/hdu_2546_01_backpack.cpp
@@ -6,16 +6,33 @@ int main() {
 	freopen("hdu_2546_01_backpack.txt", "r", stdin);
 #endif
 	int n;
-	while (scanf("%d", &n) > 1) {
+	while (scanf("%d", &n) > 0) {
 		if (n <= 0) break;
-		vector<int> price(n-1);
-		int max_val = 0;
+		vector<int> price(n);
+		int max_ind = 0;
 		for (int i = 0; i < n; i++) {
 			scanf("%d", &price[i]);
+			if (price[i] > price[max_ind]) max_ind = i;
 		}
 		int m;
 		scanf("%d", &m);
-
+		if (m < 5) {
+			printf("%d\n", m);
+			continue;
+		}
+		m -= 5;
+		int max_val = price[max_ind];
+		swap(price[max_ind], price.back());
+		price.pop_back();
+		vector<int> dp(m+1, 0);
+		for (int i = 0; i < n-1; i++) {
+			for (int j = m; j >= price[i]; j--) {
+				//必须是这种倒序，这样n和n-1才可以共用数组
+				int t = dp[j - price[i]] + price[i];
+				if (t > dp[j]) dp[j] = t;
+			}
+		}
+		printf("%d\n", m - dp[m] + 5 - max_val);
 	}
 	return 0;
 }
