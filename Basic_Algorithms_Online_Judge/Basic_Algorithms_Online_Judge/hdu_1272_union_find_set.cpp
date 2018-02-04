898648dc9013ac2443aa2a87463d0afbe021df97
LiuDongjing <zhuce201602@yeah.net>
2018-02-04 16:55:00 +0800

    并查集问题解决了

--- Basic_Algorithms_Online_Judge/Basic_Algorithms_Online_Judge/hdu_1272_union_find_set.cpp
+++ Basic_Algorithms_Online_Judge/Basic_Algorithms_Online_Judge/hdu_1272_union_find_set.cpp
@@ -2,64 +2,73 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int union_set_find(vector<int> &set, int v) {
-	return set[v] == v ? v : set[v] = union_set_find(set, set[v]);
+void reset(vector<int> & set) {
+	for (int i = 0; i < set.size(); i++) {
+		set[i] = -1;
+	}
+}
+int find(vector<int> & set, int v) {
+	if (set[v] < 0) return set[v];
+	if (set[v] == v) {
+		return v;
+	}
+	int k = find(set, set[v]);
+	set[v] = k;
+	return k;
 }
 int main() {
 #ifdef _DEBUG
 	freopen("hdu_union_find_set.txt", "r", stdin);
 #endif
 	int a, b;
+	vector<int> set(100001);
 	while (scanf("%d%d", &a, &b) > 0) {
 		if (a < 0) break;
-		if (a == 0) {
-			//printf("NO\n");
-			continue;
-		}
-		int max_val = 0;
-		vector<int> tmp;
+		reset(set);
+		int mx = a;
+		bool f = true;
 		while (a > 0) {
-			if (a > max_val) max_val = a;
-			if (b > max_val) max_val = b;
-			tmp.push_back(a);
-			tmp.push_back(b);
+			mx = max(mx, max(a, b));
+			if (f) {//保证无环
+				int x = find(set, a);
+				int y = find(set, b);
+				if (x >= 0 && y >= 0) {
+					if (x == y) {
+						f = false;
+					}
+					else {
+						set[y] = x;
+					}
+				}
+				else if (x >= 0 && y < 0) {
+					set[b] = x;
+				}
+				else if (x < 0 && y >= 0) {
+					set[a] = y;
+				}
+				else {
+					set[a] = a;
+					set[b] = a;
+				}
+			}
 			scanf("%d%d", &a, &b);
 		}
-		vector<int> set(max_val + 1);
-		for (int i = 1; i <= max_val; i++) {
-			set[i] = i;
-		}
-		bool yes = true;
-		//保证无环
-		for (int i = 0; i+1 < tmp.size(); i+=2) {
-			int p1 = union_set_find(set, tmp[i]);
-			int p2 = union_set_find(set, tmp[i + 1]);
-			if (p1 == p2) {
-				yes = false;
-				break;
-			}
-			else {
-				set[p2] = p1;
+		if (f) {
+			// 保证所有点都连通
+			int p = find(set, mx);
+			for (int i = 1; i <= mx; i++) {
+				int t = find(set, i);
+				if (t >= 0 && t != p) {
+					f = false;
+					break;
+				}
 			}
 		}
-		//保证只有一个连通集
-		for (int i = 1; i< set.size(); i++) {
-			union_set_find(set, i);
-		}
-		sort(set.begin(), set.end());
-		int c = 0;
-		for (int i = 0; i < set.size();) {
-			int j = i + 1;
-			for (; j < set.size() && set[j] == set[i]; j++);
-			if (j - i > 1) c++;
-			i = j;
-		}
-		if (c > 1) yes = false;
-		if (yes) {
-			printf("YES\n");
+		if (f) {
+			printf("Yes\n");
 		}
 		else {
-			printf("NO\n");
+			printf("No\n");
 		}
 	}
 	return 0;
